2207e6fb6a5b5f693bde4c9e8b75983ccf52c2e2
maskmo <max.askmo@gmail.com>
2023-07-23 01:01:17 -0400

    Adding my model to app.py using pickle

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import MinMaxScaler
 import tensorflow_probability as tfp
 tfd = tfp.distributions
 
+import pickle
 from pickle import dump
 from pickle import load
 
@@ -31,48 +32,90 @@ st.markdown("<body style ='color:#E2E0D9;'></body>", unsafe_allow_html=True)
 
 
 
-st.markdown("<h4 style='text-align: center; color: #1B9E91;'>House Price Prediction in Ames,Iowa</h4>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center; color: #1B9E91;'>House Price Prediction in Ames, Iowa</h4>", unsafe_allow_html=True)
 
-st.markdown("<h5 style='text-align: center; color: #1B9E91;'>A multi-step process is used to estimate the range of house prices based on your selection. The modeling process is done using the data found on Kaggle(link at left bottom corner of page)</h5>", unsafe_allow_html=True)
+st.markdown("<h5 style='text-align: center; color: #1B9E91;'>House price predictions using random forest regression, with displays of shap weights to assess importance of quantitative variables.</h5>", unsafe_allow_html=True)
 
 
-name_list = ['MSSubClass',
- 'OverallQual',
- 'YearBuilt',
- 'YearRemodAdd',
- 'BsmtUnfSF',
- 'TotalBsmtSF',
- 'FstFlrSF',
- 'SndFlrSF',
- 'GrLivArea',
- 'FullBath',
- 'HalfBath',
- 'TotRmsAbvGrd',
- 'Fireplaces',
- 'GarageCars',
- 'GarageArea',
- 'MoSold',
- 'YrSold']
+name_list = [
+    'MSSubClass',
+    'LotFrontage',
+    'LotArea',
+    'OverallQual',
+    'OverallCond',
+    'YearBuilt',
+    'YearRemodAdd',
+    'MasVnrArea',
+    'BsmtFinSF1',
+    'BsmtFinSF2',
+    'BsmtUnfSF',
+    'TotalBsmtSF',
+    'FstFlrSF',
+    'SndFlrSF',
+    'LowQualFinSF',
+    'GrLivArea',
+    'BsmtFullBath',
+    'BsmtHalfBath',
+    'FullBath',
+    'HalfBath',
+    'BedroomAbvGr',
+    'KitchenAbvGr',
+    'TotRmsAbvGrd',
+    'Fireplaces',
+    'GarageYrBlt',
+    'GarageCars',
+    'GarageArea',
+    'WoodDeckSF',
+    'OpenPorchSF',
+    'EnclosedPorch',
+    'SsnPorch',
+    'ScreenPorch',
+    'PoolArea',
+    'MiscVal',
+    'MoSold',
+    'YrSold'
+    ]
 
-name_list_train = ['MSSubClass',
- 'OverallQual',
- 'YearBuilt',
- 'YearRemodAdd',
- 'BsmtUnfSF',
- 'TotalBsmtSF',
- '1stFlrSF',
- '2ndFlrSF',
- 'GrLivArea',
- 'FullBath',
- 'HalfBath',
- 'TotRmsAbvGrd',
- 'Fireplaces',
- 'GarageCars',
- 'GarageArea',
- 'MoSold',
- 'YrSold']
+name_list_train = [
+    'MSSubClass',
+    'LotFrontage',
+    'LotArea',
+    'OverallQual',
+    'OverallCond',
+    'YearBuilt',
+    'YearRemodAdd',
+    'MasVnrArea',
+    'BsmtFinSF1',
+    'BsmtFinSF2',
+    'BsmtUnfSF',
+    'TotalBsmtSF',
+    '1stFlrSF',
+    '2ndFlrSF',
+    'LowQualFinSF',
+    'GrLivArea',
+    'BsmtFullBath',
+    'BsmtHalfBath',
+    'FullBath',
+    'HalfBath',
+    'BedroomAbvGr',
+    'KitchenAbvGr',
+    'TotRmsAbvGrd',
+    'Fireplaces',
+    'GarageYrBlt',
+    'GarageCars',
+    'GarageArea',
+    'WoodDeckSF',
+    'OpenPorchSF',
+    'EnclosedPorch',
+    '3SsnPorch',
+    'ScreenPorch',
+    'PoolArea',
+    'MiscVal',
+    'MoSold',
+    'YrSold'
+    ]
 
-data = pd.read_csv('./house-prices-advanced-regression-techniques/train.csv')
+data = pd.read_csv('./house-prices/train.csv')
 
 
 data = data[name_list_train].values
@@ -80,45 +123,82 @@ data = data[name_list_train].values
 scaler.fit(data)
 
 description_list = [
- 'What is the building class?',
- 'What is the Overall material and finish quality?',
- 'In which year was the Original construction date?',
- 'In which year was it remodelled?',
- 'What is the Unfinished square feet of basement area?',
- 'What is the Total square feet of basement area?',
- 'What is the First Floor square feet?',
- 'What is the Second floor square feet?',
- 'What is the Above grade (ground) living area square feet?',
- 'What is the number of full bathrooms?',
-'What is the number of Half baths?',
-'What is the number of  Total rooms above grade (does not include bathrooms)?',
-'What is the number of fireplaces?',
-'What is the garage capacity in car sizes?',
-'What is the size of garage in square feet?',
-'In which month was it sold?',
-'In which year was it sold?'
-
-
-
-
-
-
+    'What is the building class?',
+    'What is the Overall material and finish quality?',
+    'In which year was the Original construction date?',
+    'In which year was it remodelled?',
+    'What is the Unfinished square feet of basement area?',
+    'What is the Total square feet of basement area?',
+    'What is the First Floor square feet?',
+    'What is the Second floor square feet?',
+    'What is the Above grade (ground) living area square feet?',
+    'What is the number of full bathrooms?',
+    'What is the number of Half baths?',
+    'What is the number of  Total rooms above grade (does not include bathrooms)?',
+    'What is the number of fireplaces?',
+    'What is the garage capacity in car sizes?',
+    'What is the size of garage in square feet?',
+    'In which month was it sold?',
+    'In which year was it sold?',
+    'How many linear feet of street connected to the property?',
+    'What is the lot size in square feet?',
+    'What is the overall condition of the house from 1-10?',
+    'What is the area of veneer and masonry in square feet?',
+    'What is the type 1 finished square feet?',
+    'What is the type 2 finished square feet?',
+    'What is the low quality finished square feet?',
+    'What is the number of basement full baths?',
+    'What is the number of basement half baths?',
+    'What is the number of bedrooms above ground?',
+    'What is the number of kitchens above grade?',
+    'What is the year the garage build?',
+    'What is the area of the wood deck in square feet?',
+    'What is the open porch area in square feet?',
+    'What is the enclosed porch area in square feet?',
+    'What is the three season porch area in square feet?',
+    'What is the screen porch area in square feet?',
+    'What is the pool area in square feet?',
+    'What is the approximate dollar value of unique features?'
  ]
-min_list = [20.0,1.0,1872.0,
- 1950.0,
- 0.0,
- 0.0,
- 334.0,
- 0.0,
- 334.0,
- 0.0,
- 0.0,
- 2.0,
- 0.0,
- 0.0,
- 0.0,
- 1.0,
- 2006.0]
+
+
+
+min_list = [20.0,
+    1.0,
+    1872.0,
+    1950.0,
+    0.0,
+    0.0,
+    334.0,
+    0.0,
+    334.0,
+    0.0,
+    0.0,
+    2.0,
+    0.0,
+    0.0,
+    0.0,
+    1.0,
+    2000.0,
+    20,
+    1000,
+    1,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    1,
+    1900,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0]
 
 max_list = [190.0,
  10.0,
@@ -134,9 +214,28 @@ max_list = [190.0,
  14.0,
  3.0,
  4.0,
- 1418.0,
+ 1800.0,
  12.0,
- 2010.0]
+ 2020.0,
+ 350,
+ 250000,
+ 10,
+ 1900,
+ 2400,
+ 1200,
+ 600,
+ 3,
+ 3,
+ 6,
+ 3,
+ 2012,
+ 900,
+ 600,
+ 600,
+ 510,
+ 500,
+ 700,
+ 2500]
 
 count = 0
 
@@ -154,47 +253,70 @@ with st.sidebar:
     
 
 
-data_df = {
+data_df_obj = {
 
 'MSSubClass': [MSSubClass],
- 'OverallQual': [OverallQual],
- 'YearBuilt': [YearBuilt],
- 'YearRemodAdd': [YearRemodAdd],
- 'BsmtUnfSF': [BsmtUnfSF],
- 'TotalBsmtSF': [TotalBsmtSF],
- '1stFlrSF': [FstFlrSF],
- '2ndFlrSF': [SndFlrSF],
- 'GrLivArea':[GrLivArea],
- 'FullBath': [FullBath],
- 'HalfBath': [HalfBath],
- 'TotRmsAbvGrd':[TotRmsAbvGrd],
- 'Fireplaces': [Fireplaces],
- 'GarageCars': [GarageCars],
- 'GarageArea':[GarageArea],
- 'MoSold': [MoSold],
- 'YrSold' : [YrSold]
-
-
-
+'LotFrontage': [LotFrontage],
+'LotArea': [LotArea],
+'OverallQual': [OverallQual],
+'OverallCond': [OverallCond],
+'YearBuilt': [YearBuilt],
+'YearRemodAdd': [YearRemodAdd],
+'MasVnrArea': [MasVnrArea],
+'BsmtFinSF1': [BsmtFinSF1],
+'BsmtFinSF2': [BsmtFinSF2],
+'BsmtUnfSF': [BsmtUnfSF],
+'TotalBsmtSF': [TotalBsmtSF],
+'1stFlrSF': [FstFlrSF],
+'2ndFlrSF': [SndFlrSF],
+'LowQualFinSF': [LowQualFinSF],
+'GrLivArea': [GrLivArea],
+'BsmtFullBath': [BsmtFullBath],
+'BsmtHalfBath': [BsmtHalfBath],
+'FullBath': [FullBath],
+'HalfBath': [HalfBath],
+'BedroomAbvGr': [BedroomAbvGr],
+'KitchenAbvGr': [KitchenAbvGr],
+'TotRmsAbvGrd': [TotRmsAbvGrd],
+'Fireplaces': [Fireplaces],
+'GarageYrBlt': [GarageYrBlt],
+'GarageCars': [GarageCars],
+'GarageArea': [GarageArea],
+'WoodDeckSF': [WoodDeckSF],
+'OpenPorchSF': [OpenPorchSF],
+'EnclosedPorch': [EnclosedPorch],
+'3SsnPorch': [SsnPorch],
+'ScreenPorch': [ScreenPorch],
+'PoolArea': [PoolArea],
+'MiscVal': [MiscVal],
+'MoSold': [MoSold],
+'YrSold': [YrSold] 
 
 
 }
 
-negloglik = lambda y, p_y: -p_y.log_prob(y) # note this
+data_df = pd.DataFrame.from_dict(data_df_obj)
 
-model1 = tf.keras.models.load_model('model_files/my_keras_model1.h5')
 
-model1 = tf.keras.models.Sequential(model1.layers[:5])
+with open('model_pickle', 'rb') as f:
+    model = pickle.load(f)
 
-data_df = pd.DataFrame.from_dict(data_df)
 
-data_df_normal = scaler.transform(data_df)
+# negloglik = lambda y, p_y: -p_y.log_prob(y) # note this
 
-latent_var = model1.predict(data_df_normal)
+# model1 = tf.keras.models.load_model('model_files/my_keras_model1.h5')
 
-model2 = tf.keras.models.load_model('model_files/keras_2.h5',compile=False)
+# model1 = tf.keras.models.Sequential(model1.layers[:5])
 
-yhat = model2(latent_var)
+# data_df = pd.DataFrame.from_dict(data_df)
+
+# data_df_normal = scaler.transform(data_df)
+
+pred = model.predict(data_df)
+
+# model2 = tf.keras.models.load_model('model_files/keras_2.h5',compile=False)
+
+# yhat = model2(latent_var)
 
 
 
@@ -229,8 +351,8 @@ if center_button:
 
     col1, col2 = st.columns([3, 3])
 
-    lower_number = "{:,.2f}".format(int(yhat.mean().numpy()-1.95*yhat.stddev().numpy()))
-    higher_number = "{:,.2f}".format(int(yhat.mean().numpy()+1.95*yhat.stddev().numpy()))
+    lower_number = "{:,.2f}".format(int(pred-17496.299733333333))
+    higher_number = "{:,.2f}".format(int(pred+17496.299733333333))
 
     col1, col2, col3 = st.columns(3)
 
@@ -253,14 +375,14 @@ if center_button:
 
     
 
-    import base64
+    # import base64
 
-    file_ = open("kramer_gif.gif", "rb")
-    contents = file_.read()
-    data_url = base64.b64encode(contents).decode("utf-8")
-    file_.close()
+    # # file_ = open("kramer_gif.gif", "rb")
+    # contents = file_.read()
+    # data_url = base64.b64encode(contents).decode("utf-8")
+    # file_.close()
 
-    st.markdown(
-        f'<center><img src="data:image/gif;base64,{data_url}" alt="cat gif"></center>',
-        unsafe_allow_html=True,
-    )
+    # st.markdown(
+    #     f'<center><img src="data:image/gif;base64,{data_url}" alt="cat gif"></center>',
+    #     unsafe_allow_html=True,
+    # )
